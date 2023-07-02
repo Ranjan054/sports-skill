6add1c74d3bb6f70aafdec848661e84c4143ca3c
Ranjan054 <rk0171666@gmail.com>
2023-07-02 23:49:02 +0530

    tab view ui style add

--- js/index.js
+++ js/index.js
@@ -80,7 +80,7 @@ const activateSlider = (isCardClicked, index) => {
   phoneImg.src = `images/phoneimg-${slideIndex}.png`
   slideRightTextWrapper[slideIndex-1].style.display = "block";  
   cards[slideIndex-1].classList.add("tab-card-active");
-  activateSliderId = setTimeout(activateSlider, 2000);
+  // activateSliderId = setTimeout(activateSlider, 2000);
 }
 
 activateSlider();
